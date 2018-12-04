cde42c4b5b9b64e2d2dc44c77a3d4640d2749f64
Frederik Hermans <frederik.hermans@hotmail.com>
2018-12-04 10:29:00 +0100

    test: questions: selectNamesOfChildrenSelection and bug fixes

--- __tests__/questions_tests.js
+++ __tests__/questions_tests.js
@@ -1,4 +1,8 @@
-const { selectPictures } = require('../src/logic/questions');
+const {
+  selectPictures,
+  selectGrandParentName,
+  selectNamesOfChildrenSelection
+} = require('../src/logic/questions');
 
 describe('testing the questions: selectPictures', () => {
   const mock1 = ['6', '9'];
@@ -27,18 +31,74 @@ describe('testing the questions: selectPictures', () => {
   });
 });
 
-xdescribe('testing the questions: selectGrandParentName', () => {
-  it('should return all grandParents if no names or months have been selected', async () => {});
+describe('testing the questions: selectGrandParentName', () => {
+  const Mock_session1 = {
+    selectedNames: ['Subject1'],
+    unselectedNames: [],
+    selectedMonths: [],
+    monthOfBirth: null,
+    contactNumber: null,
+    grandParentName: null,
+    selectedPictures: [],
+    unselectedPictures: []
+  };
+  it('should return two grandParent names', async () => {
+    const result = await selectGrandParentName(Mock_session1);
+    expect(result.options.length).toEqual(2);
+  });
 
-  it('should return the right grandParents if selectedMonths passed', async () => {});
-
-  it('should return the right grandParents if selectedNames passed', async () => {});
+  it('should return the following grandParent names', async () => {
+    const result = await selectGrandParentName(Mock_session1);
+    expect(result.options).toContain('Subject6');
+    expect(result.options).toContain('Subject9');
+  });
 });
 
-xdescribe('testing the questions: selectNamesOfChildrenSelection', () => {
-  it('should return all grandParents if no names or months have been selected', async () => {});
+describe('testing the questions: selectNamesOfChildrenSelection', () => {
+  const IDs = ['6', '7', '8', '9'];
+  const Mock_session1 = {
+    selectedNames: ['Subject1'],
+    unselectedNames: [],
+    selectedMonths: [],
+    monthOfBirth: null,
+    contactNumber: null,
+    grandParentName: null,
+    selectedPictures: [],
+    unselectedPictures: []
+  };
 
-  it('should return the right grandParents if selectedMonths passed', async () => {});
+  const Mock_session2 = {
+    selectedNames: [],
+    unselectedNames: ['Subject1'],
+    selectedMonths: [],
+    monthOfBirth: null,
+    contactNumber: null,
+    grandParentName: null,
+    selectedPictures: [],
+    unselectedPictures: []
+  };
+  it('should return three names', async () => {
+    const result = await selectNamesOfChildrenSelection(IDs, Mock_session1, 3);
+    expect(result.options.length).toEqual(3);
+  });
 
-  it('should return the right grandParents if selectedNames passed', async () => {});
+  it('should contain all names except for select names', async () => {
+    const result = await selectNamesOfChildrenSelection(IDs, Mock_session1, 3);
+    expect(result.options).toContain('Subject2');
+    expect(result.options).toContain('Subject3');
+    expect(result.options).toContain('Subject4');
+  });
+
+  it('should contain all names except for in unselected names', async () => {
+    const result = await selectNamesOfChildrenSelection(IDs, Mock_session2, 3);
+    expect(result.options).toContain('Subject2');
+    expect(result.options).toContain('Subject3');
+    expect(result.options).toContain('Subject4');
+  });
+
+  it('should not contain doubles', async () => {
+    const result = await selectNamesOfChildrenSelection(IDs, Mock_session2, 3);
+    const test = result.options.every(el => result.options.indexOf(el) < 0);
+    expect(test).toBeTruthy;
+  });
 });
